f7048fbb7aeef92576c6e4f3795536698202161a
stanislas152713 <stanislas152713@gmail.com>
2025-10-07 22:15:16 -0400

    Component--of-concept Draft
    Reasons why I choose StoryGenerator, implementation of 3 methods, a demo main method

---
+++ src/components/StoryGenerator/Demo.java
@@ -0,0 +1,65 @@
+package components.StoryGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
+public final class Demo {
+
+    /**
+     * Private constructor so this utility class cannot be instantiated.
+     */
+    private Demo() {
+    }
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        StoryGenerator x = new StoryGenerator();
+        List<String> presetTemplate = new ArrayList<>();
+
+        /*
+         * Create the preset template for order
+         */
+        presetTemplate.add("Character");
+        presetTemplate.add("Action");
+        presetTemplate.add("Setting");
+        x.addTemplate("preset", presetTemplate);
+
+        /*
+         * Add elements to character
+         */
+        x.addElement("Character", "Kurt Cobain");
+        x.addElement("Character", "Axl Rose");
+        x.addElement("Character", "Damon Albarn");
+        x.addElement("Character", "Mariah Carey");
+        x.addElement("Character", "JoJo Siwa");
+
+        /*
+         * Add elements to action
+         */
+        x.addElement("Action", "sings Bette Davis Eyes");
+        x.addElement("Action", "sings Parklife");
+        x.addElement("Action", "sings Touch My Body");
+        x.addElement("Action", "sings Smells Like Teen Spirit");
+        x.addElement("Action", "sings All I Want For Christmas Is You");
+        x.addElement("Action", "sings Sweet Child O' Mine");
+
+        /*
+         * Add elements to setting
+         */
+        x.addElement("Setting", "in heaven");
+        x.addElement("Setting", "at Christmas");
+        x.addElement("Setting", "in The J team");
+        x.addElement("Setting", "in A Quiet Place");
+
+        /*
+         * Print the plot
+         */
+        System.out.print(x.generatePlot("preset"));
+    }
+}
